fe350e49aa5a0630062e92821d98b58eba935e4a
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-02-19 20:52:33 +0530

    application.kt : established db connection via jdbc

--- src/main/kotlin/com/sreshtha/Application.kt
+++ src/main/kotlin/com/sreshtha/Application.kt
@@ -7,12 +7,19 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.*
 import io.ktor.server.routing.*
+import org.ktorm.database.Database
 
 fun main(){
     embeddedServer(Netty, port = 8080, host = "0.0.0.0"){
 
         install(Routing){
             configureRouting()
+            val database = Database.connect(
+                url = "jdbc:mysql://localhost:3306/notes",
+                driver = "com.mysql.cj.jdbc.Driver",
+                user = "root",
+                password = "sample_password"
+            )
         }
 
 
